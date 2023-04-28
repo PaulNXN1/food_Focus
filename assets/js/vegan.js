ce401f229c40fb25877b08e081cab51fae064f7d
Stephen <127278005+slmov215@users.noreply.github.com>
2023-04-27 23:36:49 -0400

    Added a demo description to the topic in VeganHTML and tested an API on the JS

--- assets/js/vegan.js
+++ assets/js/vegan.js
@@ -1,2 +1,12 @@
 // API Key for fatsecretPlatformAPI
-var fatsecretPlatformAPI = '44385308c1e447f8bf9a2cea8191624f';
+var fatsecretPlatformAPIKey = '44385308c1e447f8bf9a2cea8191624f';
+//Recipe Search API 
+var edamanAPIKey = '04f42a1106f1c5216b12df8b78208f16'
+var edamanID = '8f015481'
+var dietInput // high-protein, balanced, high-fiber, low carb, low fat, low-sodium
+var healthInput // vegan, dairy-free, egg-free, gluten-free, keto-friendly, low-sugar, no-oil-added, peanut-free, sesame-free, wheat-free, tree-nut-free, vegetarian
+var excludeIngredient // exclude specific ingredient 
+var bootLeanOption // true or false
+
+
+var edaman = 'https://api.edamam.com/api/recipes/v2?type=public&app_id=' + edamanID + '&app_key=' + edamanAPIKey + '&diet='+ dietInput + '&health=' + input + '&excluded=' + excludeIngredient + '&random=' + bootLeanOption;
