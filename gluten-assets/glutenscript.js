f50904e5ba640f7ad77e014ceecd97aab3253e9c
asiabeoon <a.hashbey@gmail.com>
2023-04-30 13:16:28 -0400

    added div and script for recipe input

--- gluten-assets/glutenscript.js
+++ gluten-assets/glutenscript.js
@@ -1,45 +1,67 @@
-import { corePlugins } from '../src/corePlugins'
-import fs from 'fs'
-import path from 'path'
+// $(document).foundation();
 
-let corePluginList = Object.keys(corePlugins)
+// Function to create input box for recipe search
 
-fs.writeFileSync(
-  path.join(process.cwd(), 'src', 'corePluginList.js'),
-  `export default ${JSON.stringify(corePluginList)}`
-)
 
-var requestUrl = '';
+function recipeEl() {
+  // call area that heading will go
+  var recipeSearchEl = document.getElementById('recipe-Search');
+  // create inputbox and label
+  var recipeInputEl = document.createElement('input', search);
+  var recipeLabel =document.createElement("label");
+  // modify element h1
+  recipeLabel.textContent = "Search GF Recipes";
+ 
+  // append 
+recipeLabel.append(recipeSearchEl);
+recipeInputEl.append(recipeSearchEl);
+    }
+  
 
-var responseText = document.getElementById('response-text');
 
-function getApi(requestUrl) {
-  fetch(requestUrl)
-    .then(function (response) {
-      console.log(response);
-      if (response.status === 200) {
-        responseText.textContent = response.status;
-      }
-      // if the response status fails, then a failed message will appear
-      else{
-        responseText.textContent = "failed (" + response.status + ")";
-      }
-      return response.json();
-  });
-}
+// import { corePlugins } from '../src/corePlugins'
+// import fs from 'fs'
+// import path from 'path'
 
-getApi(requestUrl);
+// let corePluginList = Object.keys(corePlugins)
 
-window.onload = function(){
+// fs.writeFileSync(
+//   path.join(process.cwd(), 'src', 'corePluginList.js'),
+//   `export default ${JSON.stringify(corePluginList)}`
+// )
+
+// var requestUrl = '';
+
+// var responseText = document.getElementById('response-text');
+
+// function getApi(requestUrl) {
+//   fetch(requestUrl)
+//     .then(function (response) {
+//       console.log(response);
+//       if (response.status === 200) {
+//         responseText.textContent = response.status;
+//       }
+//       // if the response status fails, then a failed message will appear
+//       else{
+//         responseText.textContent = "failed (" + response.status + ")";
+//       }
+//       return response.json();
+//   });
+// }
+
+// getApi(requestUrl);
+document.querySelector('header');
+
+function headerBanner() {
 // call area that heading will go
 var heading = document.getElementById('heading');
 // create element h1 
-varh1El = document.createElement('h1');
+var h1El = document.createElement('h1');
 // modify element h1
 h1El.textContent = "A Gluten Free Life";
 h1El.setAttribute("class", "animate__bounceOut");
 // append 
-heading.appendChild(h1El);
+heading.append(h1El);
   }
 
 // window.onload=function(){
