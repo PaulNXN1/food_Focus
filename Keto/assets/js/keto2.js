c1c29c84178d4a9766995235f05b24d157281290
Evan5492 <127694249+Evan5492@users.noreply.github.com>
2023-05-02 20:55:19 -0400

    commit

--- Keto/assets/js/keto2.js
+++ Keto/assets/js/keto2.js
@@ -1,8 +1,10 @@
 let ketoBtn = document.querySelector('.keto-button');
 let answer = document.querySelector('#h2');
 
+//String of keto foods
 let foods = ['Salmon', 'Beef', 'Pork', 'Tuna', 'Eggs', 'Butter', 'Avocados', 'Cheese', 'Yogurt', 'Spinach', 'Collard Greens', 'Cabbage', 'Lettuce', 'Kale', 'Swiss Chard', 'Peppers', 'Zucchini', 'Olives','Jicama', 'Turnips', 'Asparagus', 'Broccoli', 'Mushrooms','Cucumber', 'Green Beans','Eggplants','Tomatoes', 'Radishes', 'Celery', 'Okra', 'Bacon']
 
+// Get randomn value from string and displays it
 function getRandomNumber(min,max){
     let one = max - min + 1;
     let two = Math.random() * one;
@@ -11,6 +13,7 @@ function getRandomNumber(min,max){
     return answer;
 }
 
+//Calls getRandomNumber
 ketoBtn.addEventListener('click', () => {
     let index = getRandomNumber(0, foods.length-1);
     answer.innerText = foods[index];
